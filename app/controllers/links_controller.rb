c38629a3b60ac1eb63d754aa06775dde3db4db1a
Lori Burleson <lori.burleson@gmail.com>
2016-10-17 21:31:55 -0400

    initial commit

---
+++ app/controllers/links_controller.rb
@@ -0,0 +1,28 @@
+class LinksController < ApplicationController
+  def index
+    @links = Link.all
+  end
+
+  def show
+    @link = Link.find(params[:id])
+  end
+
+  def new
+    @link = Link.new
+  end
+
+  def create
+    @link = Link.new(link_params)
+
+    if @link.save
+      redirect_to @link
+    else
+      render :new
+    end
+  end
+
+  def link_params
+    params.require(:link).permit(:title, :url)
+  end
+
+end
---
+++ spec/factories.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :link do
+    title "Testing Rails"
+    url "http://testingrailsbook.com"
+
+  end
+end
